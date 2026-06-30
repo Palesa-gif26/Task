0a7b0129bc1145a2aaa9588f8582b4fe73e690b1
palesa-gif26 <johannapalesalehutjo@gmail.com>
2026-06-30 14:24:31 +0200

    added all files

---
+++ u26683343_Prac9/Prac9_Submission/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Prac9_Submission
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtPassword = new TextBox();
+            btnCheck = new Button();
+            lblHeadng = new Label();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // txtPassword
+            // 
+            txtPassword.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtPassword.Location = new Point(12, 141);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(259, 35);
+            txtPassword.TabIndex = 0;
+            // 
+            // btnCheck
+            // 
+            btnCheck.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnCheck.Location = new Point(12, 182);
+            btnCheck.Name = "btnCheck";
+            btnCheck.Size = new Size(259, 47);
+            btnCheck.TabIndex = 1;
+            btnCheck.Text = "Check My Password";
+            btnCheck.UseVisualStyleBackColor = true;
+            btnCheck.Click += btnCheck_Click;
+            // 
+            // lblHeadng
+            // 
+            lblHeadng.AutoSize = true;
+            lblHeadng.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblHeadng.Location = new Point(12, 9);
+            lblHeadng.Name = "lblHeadng";
+            lblHeadng.Size = new Size(259, 30);
+            lblHeadng.TabIndex = 2;
+            lblHeadng.Text = "Password strength checker";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 53);
+            label1.Name = "label1";
+            label1.Size = new Size(176, 68);
+            label1.TabIndex = 3;
+            label1.Text = "Your password must contain:\r\nA capital word, \r\nA number,\r\nAnd a special character.";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(286, 251);
+            Controls.Add(label1);
+            Controls.Add(lblHeadng);
+            Controls.Add(btnCheck);
+            Controls.Add(txtPassword);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtPassword;
+        private Button btnCheck;
+        private Label lblHeadng;
+        private Label label1;
+    }
+}
---
+++ u26683343_Prac9/Prac9_Submission/Form2.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Prac9_Submission
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)axWindowsMediaPlayer1).BeginInit();
+            SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            axWindowsMediaPlayer1.Enabled = true;
+            axWindowsMediaPlayer1.Location = new Point(-2, -2);
+            axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            axWindowsMediaPlayer1.OcxState = (AxHost.State)resources.GetObject("axWindowsMediaPlayer1.OcxState");
+            axWindowsMediaPlayer1.Size = new Size(526, 678);
+            axWindowsMediaPlayer1.TabIndex = 5;
+            axWindowsMediaPlayer1.Enter += axWindowsMediaPlayer1_Enter;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            ClientSize = new Size(522, 675);
+            ControlBox = false;
+            Controls.Add(axWindowsMediaPlayer1);
+            HelpButton = true;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form2";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form2";
+            Load += Form2_Load;
+            ((System.ComponentModel.ISupportInitialize)axWindowsMediaPlayer1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+    }
+}
